22be2e6b386313b024a450664f4522d1ad50efe7
Ben Hacker <Sigabiel123@gmail.com>
2025-12-10 13:54:46 +0100

    fix: next lib set to esnext

--- examples/next/app/api/sigauth/oidc/auth/route.ts
+++ examples/next/app/api/sigauth/oidc/auth/route.ts
@@ -1,6 +1,6 @@
 import { SIGAUTH_OPTIONS } from '@/utils/constants';
 import { SigAuthNextWrapper } from '@sigauth/next';
-import { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server.js';
 
 export async function GET(req: NextRequest) {
     return await SigAuthNextWrapper.getInstance(SIGAUTH_OPTIONS).sigAuthExchange(req.url);
--- modules/next/src/next-middleware.ts
+++ modules/next/src/next-middleware.ts
@@ -1,7 +1,7 @@
 import { SigAuthHandlerResponse, SigAuthOptions, SigauthVerifier } from '@sigauth/core';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { cookies, headers } from 'next/headers.js';
-import { redirect } from 'next/navigation.js';
+import { cookies, headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 /**
  * Singleton wrapper to use SigauthVerifier within Next.js applications
